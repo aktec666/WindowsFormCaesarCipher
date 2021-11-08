6e7445f1a33cc49929fa4a675a3b7d15418c430b
aktec666 <aktec@mail.ru>
2021-11-08 10:38:14 +0300

    Initial commit

---
+++ WindowsFormCaesarCipher/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormCaesarCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCode = new System.Windows.Forms.Button();
+            this.buttonEncode = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.numericUpDownKey = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKey)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCode
+            // 
+            this.buttonCode.Location = new System.Drawing.Point(34, 125);
+            this.buttonCode.Name = "buttonCode";
+            this.buttonCode.Size = new System.Drawing.Size(160, 77);
+            this.buttonCode.TabIndex = 0;
+            this.buttonCode.Text = "Шифрование";
+            this.buttonCode.UseVisualStyleBackColor = true;
+            this.buttonCode.Click += new System.EventHandler(this.buttonCode_Click);
+            // 
+            // buttonEncode
+            // 
+            this.buttonEncode.Location = new System.Drawing.Point(223, 125);
+            this.buttonEncode.Name = "buttonEncode";
+            this.buttonEncode.Size = new System.Drawing.Size(175, 77);
+            this.buttonEncode.TabIndex = 1;
+            this.buttonEncode.Text = "Расшифрование";
+            this.buttonEncode.UseVisualStyleBackColor = true;
+            this.buttonEncode.Click += new System.EventHandler(this.buttonEncode_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(34, 44);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(364, 61);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // numericUpDownKey
+            // 
+            this.numericUpDownKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownKey.Location = new System.Drawing.Point(416, 44);
+            this.numericUpDownKey.Name = "numericUpDownKey";
+            this.numericUpDownKey.Size = new System.Drawing.Size(120, 61);
+            this.numericUpDownKey.TabIndex = 3;
+            this.numericUpDownKey.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 236);
+            this.Controls.Add(this.numericUpDownKey);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonEncode);
+            this.Controls.Add(this.buttonCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKey)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCode;
+        private System.Windows.Forms.Button buttonEncode;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.NumericUpDown numericUpDownKey;
+    }
+}
+
---
+++ WindowsFormCaesarCipher/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormCaesarCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        string Encryption(string text, int key)
+        {
+            key %= 32;
+            string result = ""; // тут будет ответ
+
+            foreach (char x in text)
+                if (alphabet.Contains((x + "").ToUpper()))
+                {
+                    string temp = alphabet[((x - 'А') + key) % 32] + "";
+                    if (x >= 'а' && x <= 'я')
+                        result += temp.ToLower();
+                    else
+                        result += temp;
+                }
+                else
+                    result += x;
+
+            return result;
+        }
+
+        string Code(string text, int key)
+        {
+            return Encryption(text, key);
+        }
+
+        string Decode(string text, int key)
+        {
+            return Encryption(text, -key);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCode_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = Code(textBoxInput.Text, (int)(numericUpDownKey.Value));
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEncode_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = Decode(textBoxInput.Text, (int)(numericUpDownKey.Value));
+        }
+    }
+}
